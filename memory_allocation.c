d86530784a350de3f28f5c8fa8fb8e4c6480e05b
Solaceisastar <105020750+Solaceisastar@users.noreply.github.com>
2022-08-11 23:26:36 +0100

    Update memory_allocation.c

--- memory_allocation.c
+++ memory_allocation.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * _realloc - reallocates a pointer to double the space.
+ * _realloc - reallocates a pointer to double the space
  * @ptr: pointer to the old array
- * @size: pointer to number of elements in the old array.
+ * @size: pointer to number of elements in the old array
  *
- * Return: pointer to the new array
+ * Return: pointer to the new array.
  */
 char **_realloc(char **ptr, size_t *size)
 {

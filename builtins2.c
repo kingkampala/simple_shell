de023e63c1807215508ae6beca7b4b3adc0fde6f
Solaceisastar <105020750+Solaceisastar@users.noreply.github.com>
2022-08-11 23:07:25 +0100

    Update builtins2.c

--- builtins2.c
+++ builtins2.c
@@ -1,8 +1,8 @@
 #include "main.h"
 /**
- * new_cd - Changes the current working directory .
- * if no parameter is passed it will change directory to HOME.
- * @vars: A string representing the input from the user.
+ * new_cd - Changes the current working directory
+ * if no parameter is passed it will change directory to HOME
+ * @vars: A string representing the input from the user
  */
 void new_cd(vars_t *vars)
 {
